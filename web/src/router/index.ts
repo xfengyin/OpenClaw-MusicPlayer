c84fb9e20710a49b6a288bd305e4fff76f777e62
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-05 23:31:50 +0800

    feat: Add router configuration

---
+++ web/src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/playlist/:id',
+    name: 'Playlist',
+    component: () => import('../views/Playlist.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import('../views/Search.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
