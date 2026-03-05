b281aacfb940ca8e85bef2780432db503fe2c554
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-05 23:23:18 +0800

    feat: Add logger utility

---
+++ server/utils/logger.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+    "log"
+    "os"
+    "time"
+)
+
+type Logger struct {
+    infoLogger  *log.Logger
+    warnLogger  *log.Logger
+    errorLogger *log.Logger
+}
+
+func NewLogger() *Logger {
+    return &Logger{
+        infoLogger:  log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
+        warnLogger:  log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile),
+        errorLogger: log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+    }
+}
+
+func (l *Logger) Info(msg string) {
+    l.infoLogger.Println(msg)
+}
+
+func (l *Logger) Warn(msg string) {
+    l.warnLogger.Println(msg)
+}
+
+func (l *Logger) Error(msg string) {
+    l.errorLogger.Println(msg)
+}
+
+func (l *Logger) Fatal(msg string) {
+    l.errorLogger.Fatal(msg)
+}
